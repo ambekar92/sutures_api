6047ec726a03a37023efdd6f35c233b8222bfeea
subramaniks <48723435+subramaniks@users.noreply.github.com>
2019-06-14 15:15:04 +0530

    Update Operator_efficiency.php
    operator efficiency date order in desc

--- Objects/Operator_efficiency.php
+++ Objects/Operator_efficiency.php
@@ -29,7 +29,7 @@ if ($emp_id == "NULL" AND $date != "NULL" ){
 }elseif($emp_id != "NULL" AND $date != "NULL" ){
     $query = "SELECT date_format(dur.datee,'%d-%m-%Y') as datee,em.emp_id,em.frst_name,ifnull(no_batch,0)as no_of_cards,ifnull(actual_duration,'00:00:00')as actual_duration,'08:00:00' as target_duration,round(ifnull(((actual_duration/SEC_TO_TIME(28800))*100),0),2) as efficiency  from tb_m_employee em
      join (SELECT date(A.updated_at) as datee,Count(A.batch_no)as no_batch,SEC_TO_TIME(SUM(TIME_TO_SEC(timediff(B.updated_at,A.updated_at)))) as actual_duration,A.emp_id,A.updated_at,B.updated_at as b_update FROM  `tb_t_job_card_trans`A 
-    JOIN   `tb_t_job_card_trans` B ON  A.oper_status = 806 and B.oper_status = 807 AND A.batch_no=B.batch_no AND A.present_mach = B.present_mach AND A.emp_id=B.emp_id AND A.sl_no<B.sl_no where A.emp_id = '$emp_id' and (date(A.updated_at) between  DATE_FORMAT('$date' ,'%Y-%m-01') AND '$date' ) GROUP BY date(A.updated_at)) dur  on dur.emp_id = em.emp_id where em.role_code = '101'";
+    JOIN   `tb_t_job_card_trans` B ON  A.oper_status = 806 and B.oper_status = 807 AND A.batch_no=B.batch_no AND A.present_mach = B.present_mach AND A.emp_id=B.emp_id AND A.sl_no<B.sl_no where A.emp_id = '$emp_id' and (date(A.updated_at) between  DATE_FORMAT('$date' ,'%Y-%m-01') AND '$date' ) GROUP BY date(A.updated_at)) dur  on dur.emp_id = em.emp_id where em.role_code = '101' order BY dur.datee DESC";
 } 
     // prepare query statement
     $stmt = $this->conn->prepare($query);
